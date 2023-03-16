f7366374a358dd3b69a4e39fd127caecb44e8f51
David Hoo <zhaihao@xwetc.cn>
2023-03-16 11:07:39 +0800

    [修改]导入方式

--- lib/easy_ui.dart
+++ lib/easy_ui.dart
@@ -4,8 +4,8 @@ export 'ui/widget/field/text_field.dart';
 export 'ui/layout/padding/padding.dart';
 export 'ui/layout/margin/margin.dart';
 export 'ui/layout/background/background.dart';
-import 'package:flutter/cupertino.dart';
-class UI {
-  Widget operator >(Widget widget) => widget;
-}
-final ui = UI();
+// import 'package:flutter/cupertino.dart';
+// class UI {
+//   Widget operator >(Widget widget) => widget;
+// }
+// final ui = UI();
